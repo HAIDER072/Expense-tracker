b562531559041536f249599e95356f877470ba98
Syed Abbas Haider Zaidi <abbashaider786000@gmail.com>
2024-10-31 21:15:51 +0530

    completed

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+// import categories from "./expense-tracker/categories";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 100, category: "utility" },
+  ]);
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-3">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="div mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
+  );
+}
+
+export default App;
